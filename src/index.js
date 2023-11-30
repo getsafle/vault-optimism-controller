33db16f727719892f82be4c6cce466ff70977905
saurabh <saurabhyadav42399@gmail.com>
2023-11-30 16:44:23 +0530

    updated getFees() and added its test

--- src/index.js
+++ src/index.js
@@ -522,10 +522,13 @@ class KeyringController extends EventEmitter {
     }
 
     async getFees(optimismTx, web3) {
-        const { from, to, value, data, gasLimit } = optimismTx
-        const estimate = gasLimit ? gasLimit : await web3.eth.estimateGas({ to, from, value, data })
+        const { from, to, value, data } = optimismTx
+        const gasLimit = await web3.eth.estimateGas({ from, to, value, data });
         const gasPrice = await web3.eth.getGasPrice();
-        return { transactionFees: estimate * gasPrice }
+        return {
+            gasLimit: gasLimit,
+            gasPrice: parseInt(gasPrice)
+        }
     }
 }
 
